109db7147a95e7ecea85cd9264c6678104743c9c
Rynnsan <sorexyazilim@gmail.com>
2026-01-10 22:28:36 +0300

    enemy slime patrol

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,12 +1,28 @@
 extends Node2D
 
-const SPEED = 60
-var direction = 1
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var speed: float = 60.0
+var direction: int = 1 # 1 sağ, -1 sol
 
+@onready var ray_right = $RayCastRight
+@onready var ray_left = $RayCastLeft
+@onready var sprite = $AnimatedSprite2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position.x += direction * SPEED* delta
+func _process(delta):
+	# Hareket: Her karede hızı ve yönü ekle
+	position.x += speed * direction * delta
+	
+	# Duvar Kontrolü
+	if direction == 1: # Sağa giderken
+		if ray_right.is_colliding(): # Sağdaki ışın duvara çarptı mı?
+			flip()
+	else: # Sola giderken
+		if ray_left.is_colliding(): # Soldaki ışın duvara çarptı mı?
+			flip()
+
+func flip():
+	direction *= -1
+	# Görseli döndür (Görselin sağa baktığını varsayıyorum)
+	if direction == 1:
+		sprite.flip_h = false
+	else:
+		sprite.flip_h = true
